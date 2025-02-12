b62391e3b7a38724e36f5e4f2e3021c713ac973c
manueljo <mikebillyy@gmail.com>
2025-02-12 15:25:05 +0100

    add search form

--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
+import React from "react";
+import SearchForm from "../../components/SearchForm";
 
-const Home = () => {
+const Home = async ({searchParams}) => {
+  const query = (await searchParams).query;
   return (
     <>
-    <section className='pink_container'>
-      <h1 className="heading">Pitch Your Startup,<br />Connect with entrepreneurs</h1>
-      <p className="sub-heading">Submit Ideas, Vote on Pitches and Get Noticed in Virtual Competitions</p>
-    </section>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup,
+          <br />
+          Connect with entrepreneurs
+        </h1>
+        <p className="sub-heading">
+          Submit Ideas, Vote on Pitches and Get Noticed in Virtual Competitions
+        </p>
+        <SearchForm query={query} />
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
